989956242a8e4363f5618f15554c54b3f6bd2847
chorn <chorn@chorn.com>
2011-10-21 12:11:57 -0400

    Now it really does something

--- lib/diabeetus.rb
+++ lib/diabeetus.rb
@@ -3,7 +3,18 @@ require "diabeetus/version"
 module Diabeetus
   class Tester
     def diabeetus?
-      self.tainted?
+      self.methods.size == self.class.new.methods.size
+    end
+
+    def insulin_shot
+      base_class_instance = self.class.new
+      self.methods.each do |m|
+        if base_class_instance.methods.grep(m).size == 0
+          class << self
+            remove_method(m)
+          end
+        end
+      end
     end
   end
 end
